a55c0a60115a8c5fba59f012c09bdecc4f4fb6aa
jay <jason.michaels721@gmail.com>
2020-08-18 06:21:58 +0000

    excluding debug log

--- wp-content/themes/fictional-university-theme/index.php
+++ wp-content/themes/fictional-university-theme/index.php
@@ -1,7 +1,8 @@
 <?php
     while (have_posts()) {
         the_post(); ?>
-        <h2><?php the_title() ?></h2>
+        <h2><a href="<?php the_perma_link(); ?>"><?php the_title() ?></a></h2>
+        <?php the_content(); ?>
         <hr />
     <?php }
 ?>
